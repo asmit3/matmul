464bae992598938757d5801fbd86f39be687f733
Saurabh Mitra <saurabh.mitra@berkeley.edu>
2016-02-05 16:25:16 -0800

    Implemented some sort of loop unrolling in preparation for vectorizing

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -12,17 +12,37 @@ const char* dgemm_desc = "Simple blocked dgemm.";
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
 static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
+  int i=0,j=0,k=0;
+  // double fa0,fb0,fa1,fb1,fa2,fb2,fa3,fb3;
+  double *Bvec;
+  double *Avec = (double*)malloc(sizeof(double)*4);
+  double cij;
   /* For each row i of A */
-  for (int i = 0; i < M; ++i)
+  for (i = 0; i < M; ++i)
     /* For each column j of B */ 
-    for (int j = 0; j < N; ++j) 
+    for (j = 0; j < N; ++j) 
     {
       /* Compute C(i,j) */
-      double cij = C[i+j*lda];
-      for (int k = 0; k < K; ++k)
-	cij += A[i+k*lda] * B[k+j*lda];
+      cij = C[i+j*lda];
+      for (k = 0; k < (K/4)*4; k+=4) {
+        // cij += A[i+k*lda] * B[k+j*lda] + A[i+(k+1)*lda] * B[k+1+j*lda] + A[i+(k+2)*lda] * B[k+2+j*lda] + A[i+(k+3)*lda] * B[k+3+j*lda];
+        Avec[0] = A[i+k*lda];
+        Avec[1] = A[i+(k+1)*lda];
+        Avec[2] = A[i+(k+2)*lda];
+        Avec[3] = A[i+(k+3)*lda];
+        Bvec = B + (k+j*lda);
+        // fb0 = B[k+j*lda];
+        // fb1 = B[k+1+j*lda];
+        // fb2 = B[k+2+j*lda];
+        // fb3 = B[k+3+j*lda];
+        cij += Avec[0]*Bvec[0] + Avec[1]*Bvec[1] + Avec[2]*Bvec[2] + Avec[3]*Bvec[3];
+      }
+      for (k=(K/4)*4; k < K; ++k) {
+        cij += A[i+k*lda] * B[k+j*lda];
+      }
       C[i+j*lda] = cij;
     }
+    free(Avec);
 }
 
 /* This routine performs a dgemm operation
@@ -46,4 +66,12 @@ void square_dgemm (int lda, double* A, double* B, double* C)
 	/* Perform individual block dgemm */
 	do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
       }
+}
+
+void transpose(int lda, double *A, double *Atrans) {
+  for (int i = 0; i < lda; i++) {
+    for (int j = 0; j < lda; j++) {
+      Atrans[j+i*lda] = A[i+j*lda];
+    }
+  }
 }
