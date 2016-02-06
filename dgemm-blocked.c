fe281a099a060fc3b914aff5db1329981a1eeca3
Saurabh Mitra <saurabh.mitra@berkeley.edu>
2016-02-05 16:40:31 -0800

    transpose completed

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -3,7 +3,7 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #if defined(BLOCK_SIZE)
 #undef BLOCK_SIZE
 #endif
-#define BLOCK_SIZE 41
+#define BLOCK_SIZE 50
 
 #define min(a,b) (((a)<(b))?(a):(b))
 
@@ -15,7 +15,7 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
   int i=0,j=0,k=0;
   // double fa0,fb0,fa1,fb1,fa2,fb2,fa3,fb3;
   double *Bvec;
-  double *Avec = (double*)malloc(sizeof(double)*4);
+  double *Avec;
   double cij;
   /* For each row i of A */
   for (i = 0; i < M; ++i)
@@ -26,23 +26,23 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
       cij = C[i+j*lda];
       for (k = 0; k < (K/4)*4; k+=4) {
         // cij += A[i+k*lda] * B[k+j*lda] + A[i+(k+1)*lda] * B[k+1+j*lda] + A[i+(k+2)*lda] * B[k+2+j*lda] + A[i+(k+3)*lda] * B[k+3+j*lda];
-        Avec[0] = A[i+k*lda];
-        Avec[1] = A[i+(k+1)*lda];
-        Avec[2] = A[i+(k+2)*lda];
-        Avec[3] = A[i+(k+3)*lda];
+        Avec = A + (k+i*lda);
         Bvec = B + (k+j*lda);
-        // fb0 = B[k+j*lda];
-        // fb1 = B[k+1+j*lda];
-        // fb2 = B[k+2+j*lda];
-        // fb3 = B[k+3+j*lda];
         cij += Avec[0]*Bvec[0] + Avec[1]*Bvec[1] + Avec[2]*Bvec[2] + Avec[3]*Bvec[3];
       }
       for (k=(K/4)*4; k < K; ++k) {
-        cij += A[i+k*lda] * B[k+j*lda];
+        cij += A[k+i*lda] * B[k+j*lda];
       }
       C[i+j*lda] = cij;
     }
-    free(Avec);
+}
+
+static void transpose(int lda, double *A, double *Atrans) {
+  for (int i = 0; i < lda; i++) {
+    for (int j = 0; j < lda; j++) {
+      Atrans[j+i*lda] = A[i+j*lda];
+    }
+  }
 }
 
 /* This routine performs a dgemm operation
@@ -51,6 +51,8 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
  * On exit, A and B maintain their input values. */  
 void square_dgemm (int lda, double* A, double* B, double* C)
 {
+  double *Atrans = malloc(sizeof(double)*lda*lda);
+  transpose(lda, A, Atrans);
   /* For each block-row of A */ 
   for (int i = 0; i < lda; i += BLOCK_SIZE)
     /* For each block-column of B */
@@ -58,20 +60,13 @@ void square_dgemm (int lda, double* A, double* B, double* C)
       /* Accumulate block dgemms into block of C */
       for (int k = 0; k < lda; k += BLOCK_SIZE)
       {
-	/* Correct block dimensions if block "goes off edge of" the matrix */
-	int M = min (BLOCK_SIZE, lda-i);
-	int N = min (BLOCK_SIZE, lda-j);
-	int K = min (BLOCK_SIZE, lda-k);
+        	/* Correct block dimensions if block "goes off edge of" the matrix */
+        	int M = min (BLOCK_SIZE, lda-i);
+        	int N = min (BLOCK_SIZE, lda-j);
+        	int K = min (BLOCK_SIZE, lda-k);
 
-	/* Perform individual block dgemm */
-	do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+        	/* Perform individual block dgemm */
+        	do_block(lda, M, N, K, Atrans + k + i*lda, B + k + j*lda, C + i + j*lda);
       }
 }
 
-void transpose(int lda, double *A, double *Atrans) {
-  for (int i = 0; i < lda; i++) {
-    for (int j = 0; j < lda; j++) {
-      Atrans[j+i*lda] = A[i+j*lda];
-    }
-  }
-}
